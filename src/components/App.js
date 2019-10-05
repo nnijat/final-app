053a4609a3816a2cda2374a28b6a85b75542e546
nnijat <nn.aca2019@gmail.com>
2019-10-04 20:51:35 -0500

    deleted currency form component file and added console.log for debug

--- src/components/App.js
+++ src/components/App.js
@@ -22,7 +22,9 @@ class App extends Component {
     }
 
     render() {
+        console.log("I am here")
         if (this.state.user.name) {
+            console.log("I am here too")
             return (
                 <div className="App">
                     <header className="App-header">
