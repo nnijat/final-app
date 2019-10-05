b1652de3c431f2d907921a2fc25d38c396ce30ae
nnijat <nn.aca2019@gmail.com>
2019-10-04 19:32:09 -0500

    update fetch url

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,7 @@ class App extends Component {
     }
 
     componentDidMount() {
-        fetch('/users')
+        fetch('https://budget-app-sn.herokuapp.com/users')
             .then(res => res.json())
             .then(data => this.setState({ user: data[0] }))
     }
